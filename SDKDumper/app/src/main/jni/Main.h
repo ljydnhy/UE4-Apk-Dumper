2f4ef51125a05efdcceb1629c1ca18300262f4ad
Sk Md Asib <98967932+Real-Ascarre@users.noreply.github.com>
2022-06-13 07:43:48 +0530

    Update Main.h

--- SDKDumper/app/src/main/jni/Main.h
+++ SDKDumper/app/src/main/jni/Main.h
@@ -1,5 +1,5 @@
 //
-// Created by Ascarre on 09-05-2022.
+// Created by Ascarre on 13-06-2022.
 //
 
 #ifndef SDKGENERATOR_MAIN_H
@@ -8,15 +8,17 @@
 #include "Dumper.h"
 #include "Canvas.h"
 
-bool StringsDumped = false, ObjectsDumped = false, SdkDumped = false, SdkWDumped = false;
-
 void RenderHacks(Canvas Draw, int SWidth, int SHeight) {
     Draw.DrawText(Color(255,40,40), "Dumper Made By Ascarre", Vector2(SWidth / 8, SHeight / 8), 25);
 
     ProcessId = FindProcessId(Offsets::ProcessName);
     ModuleBase = GetModuleBase(ProcessId, Offsets::ModuleName);
     ModuleEnd = GetModuleEnd(ProcessId, Offsets::ModuleName);
-
+    
+    if (isDumpLib) {
+        DumpLib(ModuleBase, ModuleEnd);
+    }
+    
     if (isStringDump && StringsDumped == false) {
         StringsDumped = true;
         DumpStrings(ModuleBase);
